7db74671da39941b1210d09a40f62ace63bc08bd
Hopson97 <mhopson@hotmail.co.uk>
2018-05-18 03:54:27 +0100

    fix and test Java bubble sort

--- Java/src/algorithm/Sort.java
+++ Java/src/algorithm/Sort.java
@@ -6,16 +6,14 @@ package algorithm;
  */
 public class Sort 
 {
-    static void bubbleSort(int[] data)
+    public static void bubbleSort(int[] data)
     {
         for (int i = 0; i < data.length - 1; i++) {
             for (int j = 0; j < data.length - i - 1; j++) {
-                int left = data[j];
-                int right = data[j+1];
-                if (left > right) {
-                    int temp = left;
-                    left = right;
-                    right = temp;
+                if (data[j] > data[j+1]) {
+                    int temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
                 }
             }
         }
--- Java/src/main/Main.java
+++ Java/src/main/Main.java
@@ -1,13 +1,25 @@
 package main;
 
+import algorithm.Sort;
 /**
  *
  * @author mhops
  */
 public class Main 
 {
+    public static void printArray(int[] arr) 
+    {
+        for (int val : arr) {
+            System.out.println(val);
+        }
+    }
+    
     public static void main(String[] args)
     {
+        int[] data = {1, 2, 5, 3, 4, 6, 9, 8, 7};
+        Sort.bubbleSort(data);
         
+        
+        printArray(data);
     }
 }
