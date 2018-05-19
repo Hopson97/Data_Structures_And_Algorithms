79aae63fa89dcf61f56d64f726d10e09cebc30f0
Hopson97 <mhopson@hotmail.co.uk>
2018-05-19 08:09:46 +0100

    add python insert sort

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,8 @@
-from sort_algorithms import bubble_sort
+from sort_algorithms import bubble_sort, insertion_sort
 
 if __name__ == "__main__":
     data = [5, 2, 3, 7, 8, 4, 1, 9, 6]
 
-    bubble_sort(data)
+    insertion_sort(data)
 
     print(data)
--- Python/sort_algorithms.py
+++ Python/sort_algorithms.py
@@ -6,3 +6,13 @@ def bubble_sort(data):
                 tempValue = data[j]
                 data[j] = data[j + 1]
                 data[j + 1] = tempValue
+
+def insertion_sort(data):
+    for i in range(len(data)):
+        key = data[i]
+        j   = i - 1
+
+        while j >= 0 and data[j] > key:
+            data[j + 1] = data[j]
+            j -= 1
+        data[j + 1] = key
