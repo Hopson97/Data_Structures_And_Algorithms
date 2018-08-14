1eab2426c2a8c37690f9037f0219ec7166781e99
Hopson97 <mhopson@hotmail.co.uk>
2018-08-14 23:53:22 +0100

    add function to print a list

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,2 +1,12 @@
 import Algorithm.Sort
 
+printList :: Show a => [a] -> Int -> IO ()
+printList arr n 
+    | n > 0 = do
+                _ <- print (arr !! (n - 1))
+                printList arr (n-1)
+    | otherwise = return ()
+
+main :: IO ()
+main = do
+    return ()
