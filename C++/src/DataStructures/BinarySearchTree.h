dad9ed20843fef7783bf06e56374f8a5c39e5a5d
Hopson97 <mhopson@hotmail.co.uk>
2018-11-20 14:04:59 +0000

    begin work on trees

--- C++/src/DataStructures/BinarySearchTree.h
+++ C++/src/DataStructures/BinarySearchTree.h
@@ -1,26 +1,57 @@
 #pragma once
-#include <memory>
+
+#include <iostream>
 
 template <typename T>
 class BinarySearchTree {
     struct BSTNode {
         T data;
-        std::unique_ptr<BSTNode> left = nullptr;
-        std::unique_ptr<BSTNode> right = nullptr;
+        BSTNode* left = nullptr;
+        BSTNode* right = nullptr;
     };
 
     public:
+        BinarySearchTree() {
+
+        }
+
+        ~BinarySearchTree() {
+            
+        }
+
         void insert(T data) {
             if (m_size == 0) {
-                m_rootNode = std::make_unique<BSTNode>();
-                m_size++;
+                m_rootNode = new BSTNode();
+                m_rootNode->data = data;
+                std::cout << "Root node created\n";
             }
             else {
-
+                auto newLeaf = makeLeaf(data, m_rootNode);
+                newLeaf->data = data;
+                std::cout << "Non-root node created with value: " << data << std::endl;
             }
+            m_size++;
+        }
+
+        template<typename F> 
+        void nodeForEach(F function) {
+            nodeForEach(m_rootNode, function);
         }
 
     private:
-        std::unique_ptr<BSTNode> m_rootNode = nullptr;
+        BSTNode* makeLeaf(T data, BSTNode* node) {
+            if (node == nullptr) {
+                return new BSTNode();
+            }
+
+            if (data < node->data) {
+                return makeLeaf(data, node->left);
+            }
+            else {
+                return makeLeaf(data, node->right);
+            }
+        }
+
+        BSTNode* m_rootNode = nullptr;
         size_t m_size = 0;
 };
--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -5,5 +5,7 @@
 int main() {
     BinarySearchTree<int> bst;
     bst.insert(10);
+    bst.insert(25);
+    bst.insert(30);
     return 0;
 }
